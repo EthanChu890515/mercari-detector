fda7c0815fd37fc5b763d6a8d0161c2029551209
EthanChu <154274616+EthanChu890515@users.noreply.github.com>
2026-05-28 19:45:45 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -97,7 +97,6 @@ def fetch_mercari_tw_items():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br',
         'Referer': 'https://tw.mercari.com/',
         'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
@@ -258,3 +257,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
